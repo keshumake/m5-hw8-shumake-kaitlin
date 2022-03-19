90e99a031325c16fdf743d85ece01dfcf0bd1a9a
Kaitlin Shumake <shumakek@ufl.edu>
2022-03-19 11:59:57 -0400

    amended else if statements and main function

--- index.js
+++ index.js
@@ -20,26 +20,32 @@ allBoxes.forEach(function(box) {
 // within each click event...
 // determine which box was clicked with 'this.textContent' or event.target.textContent
 // convert that value to a Number and store it to a variable
-function clickListen(event) {
-    var clickedBox = event.target.textContent();
-    var newCB = parseInt(clickedBox);
-}
-
-
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
 
-function randomNumber() {
-    var winningBox = Math.floor(Math.random() * 4);
-}
+function clickListen(event) {
+    var clickedBox = event.target.textContent();
+    var newCB = parseInt(clickedBox);
+    function randomNumber() {
+        var winningBox = Math.floor(Math.random() * 3) + 1;
 
 // determine if the box clicked is equal to the random number
 // if the numbers match, display a winning message by changing the text content of the div#message element
 // if the numbers match, increment wins and display the win count in div#wins
-if clickedBox = function randomNumber() {
-    console.log(document.getElementById('message').innerHTML('Congratulations!'));
-    console.log()
-};
+        if (newCB === winningBox) {
+            countWins++;
+            document.getElementById('message').textContent('Congratulations!'));
+            document.getElementById('wins').innerHTML(`Wins: ${countWins}`);
+        }
 
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
 // if the numbers don't match, increment losses and display the loss count in div#losses
+        else if (newCB != winningBox) {
+            userLosses++;
+            document.getElementById('losses').innerHTML(`Losses: ${userLosses}`);
+            const randomMessage = Math.floor(Math.random(losingMessage) * 3)
+            document.getElementById('message').innerHTML = losingMessage[randomMessage];
+        };
+    }
+    
+};
